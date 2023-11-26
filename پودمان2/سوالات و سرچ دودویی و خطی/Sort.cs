1c6ba247ce929ee0477e6770830369fcf59750bc
tensazangesto98754 <147942638+tensazangesto98754@users.noreply.github.com>
2023-11-26 20:56:19 +0330

    create Double sort

---
+++ پودمان2/سوالات و سرچ دودویی و خطی/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int change;
+            int[] name = { 5, 14, 3, 2, 1 };
+            int n = 5;
+            int i, j;
+            for ( i = 0; i < n-1; i++)
+            {
+                for (j = 0; j < n-i-1; j++)
+                {
+                    if (name[j] > name[j + 1])
+                    {
+                        change = name[j];
+                        name[j] = name[j +1];
+                        name[j + 1] = change;
+                    }
+
+                }
+               
+            }
+            Console.WriteLine(string.Join(", ", name));
+
+        }
+
+            
+
+         
+    }
+}
